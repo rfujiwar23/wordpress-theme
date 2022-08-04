c49dcdeaced1c41dac41c6e1a345a21ae23fff23
Ryo Fujiwara <jjcproject@icloud.com>
2022-08-04 09:38:25 +0900

    more of my wordpress theme

--- single.php
+++ single.php
@@ -6,8 +6,24 @@
 
 get_header(); ?>
 
-<p><?php the_title(); ?></p>
 
+<div class="jumbotron bg-white">
+  <?php if( have_rows('top_banner') ): ?>
+    <?php while (have_rows('top_banner')): the_row(); ?>
+      <?php if (get_row_layout() == 'top_image'):?>
+        <div class="top_banner container">
+        <div class="banner-img">
+
+          <img src="<?php echo get_sub_field('image');?>" alt="" class="img-fluid">
+        </div>
+        <div class="logo">
+          <p><?php echo get_sub_field('sub_header');?></p>
+        </div>
+        </div>
+      <?php endif;?>
+    <?php endwhile;?>
+  <?php endif;?>
+</div>
 
 <div class="container">
   <?php if( have_rows('content') ): ?>
@@ -119,10 +135,14 @@ get_header(); ?>
   <?php if(get_row_layout() == 'brand_and_items'):
     $brandText = get_sub_field('type');
     $description = get_sub_field('description');
+    $logo = get_sub_field('logo');
     $items = get_sub_field('items');
   ?>
 
     <div class="brand_and_items <?php echo $brandText;?>">
+      <div class="logo">
+        <img src="<?php echo $logo;?>" alt="<?php echo $brandText;?>" class="img-fluid">
+      </div>
       <div class="top-text">
         <h4><span><em><?php echo $brandText;?></em> Series</span></h4>
         <p><?php echo $description;?></p>
@@ -139,6 +159,7 @@ get_header(); ?>
             <?php if($item['true_or_false'] = 1):?>
               <p>900mlサイズ有</p>
             <?php endif; ?>
+
           </div>
         <?php endforeach; ?>
       </div>
@@ -159,6 +180,65 @@ get_header(); ?>
 
 
 
+</div>
+
+<div class="container">
+  <?php if( have_rows('text_and_images_content') ): ?>
+     <?php while (have_rows('text_and_images_content')): the_row(); ?>
+        <?php if (get_row_layout() == 'free_text'):?>
+
+          <div class="text-contents">
+            <h3 class="text-center"><?php echo get_sub_field('title');?></h3>
+            <p><?php echo get_sub_field('content');?></p>
+          </div>
+
+        <?php endif; ?>
+         <?php if (get_row_layout() == 'body_banner_image'):?>
+
+          <img src="<?php echo get_sub_field('banner_image');?>" alt="banner image" class="img-fluid">
+
+        <?php endif; ?>
+
+        <?php if (get_row_layout() == 'two_column_img'): ?>
+          <div class="col-lg-8 offset-lg-2 col-md-8 offset-md-2 col-sm-8 offset-sm-2 col-10 offset-1">
+            <div class="row">
+            <div class="col-6"><img src="<?php echo get_sub_field('left_image');?>" alt="banner image" class="img-fluid"></div>
+            <div class="col-6"><img src="<?php echo get_sub_field('right_image');?>" alt="banner image" class="img-fluid"></div>
+            </div>
+          </div>
+        <?php endif; ?>
+
+        <?php if(get_row_layout() == 'body_link_two_columns'):
+            $bodyLinks = get_sub_field('go_to_link');
+          ?>
+
+          <div class="two-column-image-link col-lg-8 offset-lg-2 col-md-8 offset-md-2 col-sm-8 offset-sm-2">
+            <div class="row">
+            <?php foreach($bodyLinks as $bodyLink):?>
+            <div class="col-6">
+              <a href="<?php echo $bodyLink['image'] ?>">
+              <div class="bodyLink card h-100">
+                <img src="<?php echo $bodyLink['image'] ?>" alt="<?php echo $bodyLink['heading']; ?>" class="img-fluid card-img">
+                <div class="text card-body">
+                  <h5><span><?php echo $bodyLink['heading']; ?></span></h5>
+                  <p><?php echo $bodyLink['short_description']; ?></p>
+                </div>
+              </div>
+            </a>
+            </div>
+          <?php endforeach; ?>
+          </div>
+
+          </div>
+
+        <?php endif; ?>
+
+
+
+
+
+     <?php endwhile; ?>
+  <?php endif; ?>
 </div>
 
 
